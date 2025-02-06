86d5dc96e96615cdab97a88023fbf45f711b79bf
vibranta-tech <vibranta.care@gmail.com>
2025-02-06 17:32:30 +0530

    Delete accounts directory

--- accounts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms # type: ignore
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    class Meta:
-        model = User
-        fields = ['username', 'email','password1', 'password2']
